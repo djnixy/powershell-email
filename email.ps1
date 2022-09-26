521228d6cc5c6856c22fa1fdc795029dbdbaa58b
Niki Akbar <djnixy@users.noreply.github.com>
2022-09-26 21:35:20 +0700

    Update email.ps1

--- email.ps1
+++ email.ps1
@@ -1,11 +1,11 @@
-$Body = "Body"  
+$MailBody = "Body"  
 $SmtpServer = 'smtp.gmail.com'  
-$SmtpUser = $smtpUser
-$smtpPassword = $smtpPassword
-$MailFrom = $smtpFrom  
-$MailtTo = $smtpTarget  
-$MailSubject = $emailSubject 
-$Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $SmtpUser, $($smtpPassword | ConvertTo-SecureString -AsPlainText -Force)  
-Send-MailMessage -To "$MailtTo" -from "$MailFrom" -Subject $MailSubject -Body "$Body" -SmtpServer $SmtpServer -BodyAsHtml -UseSsl -Credential $Credentials -port 587
+$SmtpUser = $SmtpUser
+$SmtpPassword = $SmtpPassword
+$MailFrom = $MailFrom  
+$MailTo = $MailTo
+$MailSubject = $MailSubject 
+$Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $SmtpUser, $($SmtpPassword | ConvertTo-SecureString -AsPlainText -Force)  
+Send-MailMessage -To "$MailTo" -from "$MailFrom" -Subject $MailSubject -Body "$MailBody" -SmtpServer $SmtpServer -BodyAsHtml -UseSsl -Credential $Credentials -port 587
 #with attachment below
 #Send-MailMessage -To "$MailtTo" -from "$MailFrom" -Subject $MailSubject -Body "$Body" -Attachments ".\SampleAttachment.txt" -SmtpServer $SmtpServer -BodyAsHtml -UseSsl -Credential $Credentials -port 587
